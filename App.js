117367e2c1462c9fe5e133447e1ac3129e901550
jaswanth <ajaswanth791@gmail.com>
2026-02-11 07:45:11 +0530

    feat: first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+// const element = React.createElement("h1",{
+//     id:"heading"
+// },"Hello, world");
+// console.log("element is ", element);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log("root is ", root);
+
+// root.render(element);
+
+/**
+ * Now we need to create the nested html
+ * 
+ * <div id="parent">
+ *     <div id="child">
+ *          <h1>Hello, world!</h1>
+ *     </div>
+ * </div>
+ * 
+ */
+
+const parent = React.createElement(
+    "div",
+    {
+        id:"parent"
+    },
+    React.createElement(
+        "div",
+        {
+            id:"child"
+        },
+        React.createElement(
+            "h1",
+            {
+                id:"h1"
+            },
+            "Hello, world!"
+        )
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
